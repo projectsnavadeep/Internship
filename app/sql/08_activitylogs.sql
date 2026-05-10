0881f3de3e0c746b4f1d24f96d2fb0485694e8e2
Guntukasanjana <guntukaravi46@gmail.com>
2026-05-10 16:44:51 +0530

    Update 08_activitylogs.sql

--- app/sql/08_activitylogs.sql
+++ app/sql/08_activitylogs.sql
@@ -1,5 +1,5 @@
 UPDATE public.activity_logs
-SET metadata = jsonb_set(
+SET metadata = jsonb_set( 
   COALESCE(metadata, '{}'::jsonb), 
   '{session_id}', 
   to_jsonb(upper(to_char(date(created_at), 'MONDD')) || '(' || upper(substring(md5(user_id::text || date(created_at)::text), 1, 4)) || ')')
